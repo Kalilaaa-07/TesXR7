4cd746a031d82bc11efa7bb1109aa1f3e4df6d8a
Kalilaaa <kalilah_arafah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:58:13 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,6 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Kalila dari kelas xrpl 7 yang berasal dari Jombang  ");
+        String a = "1";
+    }
+}
