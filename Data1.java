8d835d0648f3362a70a1d2ad0f71863030fd32b7
Kalilaaa <kalilah_arafah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:35 +0700

    Add files via upload

---
+++ Data1.java
@@ -0,0 +1,13 @@
+public class Data1 {
+    public static void main(String[] args) {
+        String nama = "Kalila";
+        int usia = 15;
+        String alamat = "Isekai";
+        double kelas = 10.7;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Usia : " + usia);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
