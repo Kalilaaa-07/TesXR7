f5073070486d6a4255180300aac38207e277f1bf
Kalilaaa <kalilah_arafah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:46:40 +0700

    Add files via upload

--- KaryawanTester.java
+++ KaryawanTester.java
@@ -1,21 +1,37 @@
+import java.util.Scanner;
+
 public class KaryawanTester{
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Nama: ");
+        String nama = input.nextLine();
+        System.out.println("Input Unit: ");
+        String unit = input.nextLine();
+        System.out.println("Input Alamat: ");
+        String alamat = input.nextLine();
+        System.out.println("Input ID: ");
+        int id = input.nextInt();
+
+        Karyawan kosong = new Karyawan(id, nama, alamat, unit);
+        kosong.print();
         
-        Karyawan keyza = new Karyawan(10, "Keyza", "Tulungagung", "BK");
-        Karyawan aveline =  new Karyawan(15, "Aveline", "Tulungagung", "TU");
-        Karyawan nur = new Karyawan(20, "Memey", "Banyuwangi", "Kesiswaan");
-        Karyawan leni = new Karyawan(25, "Leni", "Malang", "UKS");
-        Karyawan ninmas = new Karyawan(30, "Nimas", "Malang", "Perpus");
+//         Karyawan keyza = new Karyawan(10, "Keyza", "Tulungagung", "BK");
+//         Karyawan aveline =  new Karyawan(15, "Aveline", "Tulungagung", "TU");
+//         Karyawan nur = new Karyawan(20, "Memey", "Banyuwangi", "Kesiswaan");
+//         Karyawan leni = new Karyawan(25, "Leni", "Malang", "UKS");
+//         Karyawan ninmas = new Karyawan(30, "Nimas", "Malang", "Perpus");
     
-        keyza.print();
-        System.out.println();
-        aveline.print();
-        System.out.println();
-        nur.print();
-        System.out.println();
-        leni.print();
-        System.out.println();
-        ninmas.print();
-        System.out.println();
-}
+//         keyza.print();
+//         System.out.println();
+//         aveline.print();
+//         System.out.println();
+//         nur.print();
+//         System.out.println();
+//         leni.print();
+//         System.out.println();
+//         ninmas.print();
+//         System.out.println();
+input.close();
+    }
 }
